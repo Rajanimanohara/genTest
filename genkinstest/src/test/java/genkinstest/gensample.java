1a80ed6a6215bc2273b6d5d2440c9e9fe43d3218
Rajanimanohara <rajjumr@gmail.com>
2023-03-10 17:16:15 +0530

    1st commmit

---
+++ genkinstest/src/test/java/genkinstest/gensample.java
@@ -0,0 +1,15 @@
+package genkinstest;
+
+import org.testng.annotations.Test;
+
+public class gensample {
+	
+	@Test
+public void genTest(){
+		int a=3;
+		System.out.println("executing");
+	}
+
+
+
+}
